a8a0c135b24754727806c4e433aac5ac7a4d6122
Zhang <504947985@qq.com>
2022-03-24 10:51:50 +0800

    增加了路径检测功能，修复文件已打开的bug

--- combineExcel.py
+++ combineExcel.py
@@ -1,8 +1,14 @@
 import os
 import sys
+import json
 import time
+import hashlib
 import openpyxl
+import warnings
 import traceback
+
+
+warnings.filterwarnings('ignore')
 
 if not os.path.exists('setting.py'):
     with open('setting.py', 'w') as fl:
@@ -32,7 +38,7 @@ MODE 表示模式选择，目前只有 0 和 1 两种模式：
     os.system('pause')
     sys.exit()
 
-from setting import FILE_SAVE_PATH, NEED_COMBINE_DICTORY_PATH, MODE
+from setting import *
 
 if MODE not in (0, 1):
     print('请在 setting.py 中设置正确的 MODE 值，')
@@ -41,41 +47,77 @@ if MODE not in (0, 1):
     sys.exit()
 
 
+def md5_text(text):
+    md5 = hashlib.md5()
+    md5.update(text.encode())
+    return md5.hexdigest()
+
+def safe_remove(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+class JsonDb(dict):
+
+    @classmethod
+    def from_file(cls, path):
+        with open(path, 'r', encoding='utf-8')as fl:
+            data = json.load(fl)
+        return cls(data)
+
+    def save(self, path):
+        with open(path, 'w', encoding='utf-8')as fl:
+            json.dump(self, fl, ensure_ascii=False, indent=4)
+
+
 class CombineExcelFiles:
 
     def __init__(self):
 
-        self.current_time = time.strftime('%Y-%m-%d-%H-%M-%S')
-        self.default_path = os.getcwd()
-        self.filename = '合并完成({}).xlsx'.format(self.current_time)
-        if len(FILE_SAVE_PATH) == 0:
-            self.file_path = os.path.join(self.default_path, self.filename)
-        else:
-            self.file_path = os.path.join(FILE_SAVE_PATH, self.filename)
-        if len(NEED_COMBINE_DICTORY_PATH) == 0:
-            self.target_path = self.default_path
-        else:
-            self.target_path = NEED_COMBINE_DICTORY_PATH
+        self.filename = '合并完成.xlsx'
+        self.log_file = LOG_FILE
+        self.save_file = os.path.join(FILE_SAVE_PATH, self.filename)
+        self.combine_file_path = NEED_COMBINE_DICTORY_PATH
 
     def run(self, mode=MODE):
         try:
-            self.createExcel()
-            self.combine(mode)
+            if not os.path.exists(self.save_file) or not os.path.exists(self.log_file):
+                safe_remove(self.save_file)
+                safe_remove(self.log_file)
+                self.log_flag = False
+                self.log_data = JsonDb()
+                self.log_data['path_md5'] = []
+                self.create_excel()
+                self.combine(mode)
+            else:
+                print('正在加载初始表格...')
+                self.load_excel(self.save_file)
+                print('初始表格加载完成。')
+                self.log_flag = True
+                self.log_data = JsonDb.from_file(self.log_file)
+                self.combine(mode)
+
         except Exception as e:
             traceback.print_exc()
             os.system('pause')
         finally:
             self.close()
 
-    def createExcel(self):
+    def create_excel(self):
         self.wb = openpyxl.Workbook()
         self.ws = self.wb.active
         self.max_row = 0
         self.max_column = 0
 
-    def load_and_combine(self, Excel_abs_path, mode=MODE):
+    def load_excel(self, path):
+        self.wb = openpyxl.load_workbook(path)
+        self.ws = self.wb.active
+        self.max_row = self.ws.max_row
+        self.max_column = self.ws.max_column
 
-        wb = openpyxl.load_workbook(Excel_abs_path)
+    def load_and_combine(self, excel_abs_path, mode=MODE):
+
+        wb = openpyxl.load_workbook(excel_abs_path)
         ws = wb.active
         # 得到目标表格最大的行列数
         max_row = ws.max_row
@@ -108,20 +150,28 @@ class CombineExcelFiles:
         # 不同的 mode 代表着不同的表格合并策略
         # 默认 0 代表着全合并
         # 数字 1 代表着除了第一个表格，其他表格忽略掉第一行
-        target_file_list = os.listdir(self.target_path)
-        for root, dirs, files in os.walk(self.target_path):
+        target_file_list = os.listdir(self.combine_file_path)
+        for root, dirs, files in os.walk(self.combine_file_path):
             for file in files:
                 if file.endswith('.xlsx') and '合并' not in file:
-                    Excel_abs_path = os.path.join(root, file)
-                    print(Excel_abs_path)
-                    self.load_and_combine(Excel_abs_path)
+                    excel_abs_path = os.path.join(root, file)
+                    excel_abs_path_md5 = md5_text(excel_abs_path)
+                    if excel_abs_path_md5 not in self.log_data['path_md5']:
+                        print(excel_abs_path)
+                        self.load_and_combine(excel_abs_path)
+                        self.log_data['path_md5'].append(excel_abs_path_md5)
 
     def close(self):
-        self.wb.save(self.file_path)
-        self.wb.close()
+        try:
+            self.wb.save(self.save_file)
+            self.log_data.save(self.log_file)
+        except PermissionError:
+            print('保存失败！！！\n')
+            print(f'请先关闭[{self.save_file}]才能正常保存。')
+        finally:
+            self.wb.close()
 
 if __name__ == '__main__':
-    # pass
     print('提示：只有后缀名为 xlsx 的才能合并。')
     print('正在合并工作表...\n')
 
@@ -132,6 +182,6 @@ if __name__ == '__main__':
 
     print('\n合并完成。共耗时 {} 秒。\n'.format(end_time - start_time))
     print('目标文件位置为：')
-    print(c.file_path)
+    print(c.save_file)
     print()
     os.system('pause')
